30d48086357c57f29d09232c5c42138a7cb3bf60
Pedro Moraes <pedrowebedesign@gmail.com>
2025-03-29 13:31:54 -0300

    build: project bases

---
+++ constructor-injection/src/main/java/com/practicing/core/domains/App.java
@@ -0,0 +1,12 @@
+package com.practicing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringApplication
+public class App {
+
+    public static void main(String args[]){
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ constructor-injection/src/main/java/com/practicing/core/domains/coaches/models/CrickectCoach.java
@@ -0,0 +1,8 @@
+package com.practicing.core.domains.coaches.models;
+
+public class CrickectCoach implements Coach {
+
+    public String getDailyWorkout(){
+        return "Do 10m warmups before the 10m running and 20m of ball control";
+    }
+}
